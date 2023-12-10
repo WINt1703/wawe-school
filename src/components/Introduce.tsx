9d7ba1ca2cdb61206e97619334fb1d61828be45f
VoropaievOleksandr <isuszanty@gmail.com>
2023-12-10 20:38:53 +0100

    fixed: changed h-screen to h-full

--- src/components/Introduce.tsx
+++ src/components/Introduce.tsx
@@ -4,10 +4,10 @@ import Surfing from "../assets/surfing-wawe.png"
 
 const Introduce: FC = () => {
   return (
-    <div className="relative w-full">
-      <img className="absolute h-screen w-full object-cover" src={Surfing} alt="surfing" />
+    <div className="relative h-full w-full">
+      <img className="absolute max-h-screen w-full object-cover" src={Surfing} alt="surfing" />
       <div className="absolute h-48 w-full bg-gradient-to-b from-[#01353E]" />
-      <div className="absolute flex h-screen w-full flex-col items-stretch pt-5">
+      <div className="absolute flex h-full w-full flex-col pt-5">
         <div className="flex items-center justify-center gap-x-14 text-white">
           <img src={Logo} className="h-10 w-10" alt="logo" />
           <div className="decoration-[#00B2A0] underline-offset-4 hover:underline">About us</div>
@@ -16,7 +16,7 @@ const Introduce: FC = () => {
           <div>Team</div>
           <div>Contacts</div>
         </div>
-        <p className="m-auto text-center leading-tight pb-16">
+        <p className="flex-grow grid place-content-center mb-20 text-center leading-tight">
           <div className="font-raleway text-[200px] font-700 uppercase text-[#0D302C]">wawe</div>
           Surf school in the Canary Islands
         </p>
