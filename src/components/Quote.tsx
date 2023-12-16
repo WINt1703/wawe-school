aec88023e5cb4f14f9382791577eb8a8ac4a3f4e
VoropaievOleksandr <isuszanty@gmail.com>
2023-12-16 01:29:26 +0100

    added: component Quote

---
+++ src/components/Quote.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react"
+
+const Quote: FC = () => {
+  return (
+    <p className="text-center text-3xl">
+      «When in doubt, go for a ride.»
+      <div className="indent-96 text-xs text-stone-400">Nat Young, first World Surfing Champion</div>
+    </p>
+  )
+}
+
+export default Quote
