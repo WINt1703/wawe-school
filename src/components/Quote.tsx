c4019fcb318be9ed57e2c381086f84997422ae2c
VoropaievOleksandr <isuszanty@gmail.com>
2023-12-16 01:43:44 +0100

    fixed: changed style

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -4,7 +4,7 @@ const Quote: FC = () => {
   return (
     <p className="text-center text-3xl">
       «When in doubt, go for a ride.»
-      <div className="indent-96 text-xs text-stone-400">Nat Young, first World Surfing Champion</div>
+      <div className="description indent-96 text-stone-400">Nat Young, first World Surfing Champion</div>
     </p>
   )
 }
