efbce8caf69f585ef1aa68902b9c643cd2760425
VoropaievOleksandr <isuszanty@gmail.com>
2023-12-16 02:49:37 +0100

    removed: removed dotenv

--- src/utils/hooks/fetcher.ts
+++ src/utils/hooks/fetcher.ts
@@ -1,5 +1,5 @@
 async function fetcher<TResponse>(file: string): Promise<TResponse> {
-  return await fetch(process.env.PUBLIC_URL + file).then((response) => response.json())
+  return await fetch("http://localhost:3001" + file).then((response) => response.json())
 }
 
 export default fetcher
