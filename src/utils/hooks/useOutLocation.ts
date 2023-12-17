76db8a089ec31637cbea21ea4aef954e7150cd8a
VoropaievOleksandr <isuszanty@gmail.com>
2023-12-17 23:16:20 +0100

    added: component OutLocation

---
+++ src/utils/hooks/useOutLocation.ts
@@ -0,0 +1,32 @@
+import useSWR, { SWRResponse } from "swr"
+import fetcher from "./fetcher"
+
+interface OurLocation {
+  zoomCenter: Location
+  ourLocation: Location
+  surfingHousePhoto: string
+}
+
+interface Location {
+  lat: number
+  lng: number
+}
+
+type ReturnLocationType = Omit<SWRResponse, "data"> & { location?: OurLocation }
+
+function useOutLocation(): ReturnLocationType {
+  const { data, error, isLoading, isValidating, mutate } = useSWR<OurLocation>(
+    "/location",
+    fetcher
+  )
+
+  return {
+    error: error,
+    location: data,
+    isLoading: isLoading,
+    isValidating: isValidating,
+    mutate: mutate,
+  }
+}
+
+export default useOutLocation
