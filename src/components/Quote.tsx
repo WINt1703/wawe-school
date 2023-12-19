463a193aad3dd2a9bbc570a7ef828f273460da02
VoropaievOleksandr <isuszanty@gmail.com>
2023-12-19 19:06:49 +0100

    fixed: added responsive design in Quote

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -2,9 +2,11 @@ import { FC } from "react"
 
 const Quote: FC = () => {
   return (
-    <p className="text-center text-3xl">
+    <p className="text-center text-2xl md:text-3xl">
       «When in doubt, go for a ride.»
-      <div className="description indent-96 text-stone-400">Nat Young, first World Surfing Champion</div>
+      <div className="description text-stone-400 md:indent-96">
+        Nat Young, first World Surfing Champion
+      </div>
     </p>
   )
 }
