a1b5b228f535e07393d1447cd7d7587d766e1cd3
VoropaievOleksandr <isuszanty@gmail.com>
2023-12-19 19:36:13 +0100

    fixed: added responsive design in Team

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -8,7 +8,7 @@ const Team: FC = () => {
     team && (
       <div className="space-y-12">
         <p className="title">Out team</p>
-        <div className="flex justify-center gap-x-24">
+        <div className="flex flex-col items-center justify-center gap-x-24 gap-y-8 lg:flex-row">
           {team.map((t) => (
             <Member
               key={t.id}
