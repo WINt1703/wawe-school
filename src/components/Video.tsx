f5edbd617a90ec9928b820e3126e0d563d5e0f03
VoropaievOleksandr <isuszanty@gmail.com>
2023-12-19 19:06:57 +0100

    fixed: added responsive design in Video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react"
 
-type VideoProps = {
-  width: number
-  height: number
-}
-
-const Video: FC<VideoProps> = (props) => {
-  return <iframe {...props} className="rounded-md" src="https://www.youtube.com/embed/ma67yOdMQfs" title="These Were The All-Time Surfing Moments Of The Year | Best Of 2020" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+const Video: FC = () => {
+  return (
+    <iframe
+      className="h-full max-h-96 w-3/4 max-w-7xl rounded-md"
+      src="https://www.youtube.com/embed/ma67yOdMQfs"
+      title="These Were The All-Time Surfing Moments Of The Year | Best Of 2020"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    ></iframe>
+  )
 }
 
 export default Video
