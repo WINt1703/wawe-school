45bc50b478fee7112d84660f6599bc1905f2458c
VoropaievOleksandr <isuszanty@gmail.com>
2023-12-20 00:36:57 +0100

    fixed: initial category

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,21 +1,26 @@
-import { FC, useState } from "react"
+import { FC, useEffect, useState } from "react"
 import useGallery from "../utils/hooks/useGallery"
 import Button from "./Button"
 
 const Gallery: FC = () => {
   const { gallery } = useGallery()
-  const [category, setCategory] = useState<string | undefined>(
-    gallery?.currentCategory ?? gallery?.categories[0].name
-  )
+  const [category, setCategory] = useState<string | undefined>()
   const photos = gallery?.categories.find((c) => c.name === category)?.photos
   const categoryHandler = (name: string): void => setCategory(name)
 
+  useEffect(() => {
+    if (gallery) {
+      setCategory(gallery.currentCategory)
+    }
+  }, [gallery])
+
   return (
-    gallery && (
+    gallery &&
+    photos && (
       <div className="mx-auto grid place-content-center space-y-8">
         <div className="title">Gallery</div>
         <div className="flex gap-x-5">
-          {gallery?.categories.map((c, i) => (
+          {gallery.categories.map((c, i) => (
             <div
               key={i}
               className={`uppercase hover:cursor-pointer ${
@@ -27,21 +32,19 @@ const Gallery: FC = () => {
             </div>
           ))}
         </div>
-        {photos && (
-          <div className="grid max-w-6xl grid-cols-4 gap-10">
-            {photos.map((p, i) => (
-              <img
-                src={p}
-                alt=""
-                className={`h-52 w-full object-cover ${
-                  (i + 1) % 6 === 1 || !((i + 1) % 6)
-                    ? "col-span-2"
-                    : "col-span-1"
-                }`}
-              />
-            ))}
-          </div>
-        )}
+        <div className="grid max-w-6xl grid-cols-4 gap-10">
+          {photos!.map((p, i) => (
+            <img
+              src={p}
+              alt=""
+              className={`h-52 w-full object-cover ${
+                (i + 1) % 6 === 1 || !((i + 1) % 6)
+                  ? "col-span-2"
+                  : "col-span-1"
+              }`}
+            />
+          ))}
+        </div>
         <Button type="button" className="mx-auto">
           Show more
         </Button>
